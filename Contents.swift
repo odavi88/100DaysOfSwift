7a4b59768f4494fdb4b8720bcc3a64ebfb505e3a
Omar D <omardavidson227@gmail.com>
2022-12-23 04:24:14 -0500

    added new variables and add a quote inside my strings

--- Contents.swift
+++ Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-// Hacking with Swift Tutorial 1
+// MARK: Hacking with Swift Tutorial 1
 //How to create variables and constants
 
-// Variables
+// MARK: Variables
 var greeting = "Hello, world!"
 
 var name = "Darth Vader"
@@ -11,14 +11,21 @@ var name = "Darth Vader"
 name = "Luke"
 name = "Leia"
 
+//MARK: Constants
 let character = "Anikin"
 
 print(character)
 print(name)
 print(greeting)
 
-// Use Constants over Variables when possible to optimize your code and stops us from changing values on accident.
+//NOTE: Use Constants over Variables when possible to optimize your code and stops us from changing values on accident.
 
+//MARK: How to Create Strings
+
+let saying = "All for one and one for all."
+let quote = "He said, \"You can make a difference.\""
+
+print(quote)
 
 
 

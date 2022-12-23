bea3afa524f9d7d3244153997240a956194f568a
Omar D <omardavidson227@gmail.com>
2022-12-23 03:55:07 -0500

    added new variable and created instances of that variable

--- Contents.swift
+++ Contents.swift
@@ -5,7 +5,12 @@ var greeting = "Hello, playground"
 // Hacking with Swift Tutorial 1
 //How to create variables and constants
 
-var greeting = "Hello, world!" 
+var greeting = "Hello, world!"
+
+var name = "Darth Vader"
+
+name = "Luke"
+name = "Leia"
 
 
 
